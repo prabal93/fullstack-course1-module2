fc17b9cf40341902798a1eafd79e1deb7ce73156
prabal93 <prabalchauhan33@gmail.com>
2016-06-23 12:38:08 +0530

    Update module2_lesson1_formative.rb

--- Assignments/Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
+++ Assignments/Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
@@ -1,15 +1,19 @@
-some_var = "false"
-another_var = "nil"
+some_var="false"
+another_var="nil"
 
-if some_var == "pink elephant"
-  puts "Don't think about the pink elephant!"
+puts case
 
-elsif another_var.nil?
-  puts "Question mark in the method name?"
-
-elsif some_var == false
-  puts "Looks like this one should execute"
-
+when some_var="pink elephant"
+	puts "Don't think about the pink elephant!"
+when another_var.nil?
+	puts "Question mark in the method name?"
+when some_var=false
+	puts "Looks like this one should execute"
 else
-  puts "I guess nothing matched... But why?"
-end
+	puts "I guess nothing matched... But why?"
+
+
+		end
+
+
+		
